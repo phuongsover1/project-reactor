aae9759d4b2189b50e4e6bd1e873370a96ea8114
Phuong Nguyen <phuongsover2@gmail.com>
2024-05-29 22:14:10 +0700

    add gitignore file

--- src/test/java/com/projectreactor/MonoTest.java
+++ src/test/java/com/projectreactor/MonoTest.java
@@ -5,6 +5,22 @@ import org.junit.jupiter.api.Test;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+/**
+ * Reactive Streams
+ * 1. Asynchronous
+ * 2. Non-blocking
+ * 3. Backpressure
+ * Publisher <- (subcribe) Subscriber
+ * Subscription is created
+ * Publisher (onSubscribe with the subscription) -> Subscriber
+ * Subscription <- (request N) Subscriber
+ * until:
+ * 1. Publisher sends all the objects requested.
+ * 2. Publisher sends all the objects it has. (onComplete) subscriber and
+ * subscription will be canceled
+ * 3. There is an error. (onError) -> subsciber and subscription will be
+ * canceled
+ */
 public class MonoTest {
     @Test
     public void test() {
