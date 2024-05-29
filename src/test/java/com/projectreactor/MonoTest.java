189d482a9e09992f7f2edecc88fb999fe8e222d8
Phuong Nguyen <phuongsover2@gmail.com>
2024-05-29 22:03:11 +0700

    initial setup

---
+++ src/test/java/com/projectreactor/MonoTest.java
@@ -0,0 +1,13 @@
+package com.projectreactor;
+
+import org.junit.jupiter.api.Test;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class MonoTest {
+    @Test
+    public void test() {
+        log.info("Everthing working as intended!!!");
+    }
+}
