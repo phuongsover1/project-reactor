f713208a8ced5c6f69a42ec7e2da68cbf83c799e
Phuong Nguyen <phuongsover2@gmail.com>
2024-06-03 12:34:21 +0700

    Subscribing to Flux

---
+++ src/test/java/com/projectreactor/FluxTest.java
@@ -0,0 +1,33 @@
+package com.projectreactor;
+
+import org.junit.jupiter.api.Test;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+@Slf4j
+public class FluxTest {
+    @Test
+    public void fluxSubscriber() {
+        Flux<String> fluxString = Flux.just("William", "Suane", "DevDojo", "Academy")
+                .log();
+        StepVerifier.create(fluxString)
+                .expectNext("William", "Suane", "DevDojo", "Academy")
+                .verifyComplete();
+    }
+
+    @Test
+    public void fluxSubsciberNumbers() {
+        Flux<Integer> flux = Flux.range(1, 5)
+                .log();
+        flux.subscribe(i -> log.info("Number {}", i));
+
+        log.info("=========================");
+        StepVerifier.create(flux)
+                .expectNext(1, 2, 3, 4, 5)
+                .verifyComplete();
+
+    }
+
+}
